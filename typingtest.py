a28de3c46afff9283e3de055b3548616521436cf
Shamitbh <shamit.bhatia2@gmail.com>
2022-07-28 17:43:34 -0700

    Created starting screen welcome message function

--- typingtest.py
+++ typingtest.py
@@ -1,10 +1,22 @@
 import curses
 from curses import wrapper # used to iniitalize curses module and then restore state after finished
 
-def main(stdscr):
-    stdscr.clear() # clears screen from last run
-    stdscr.addstr("Hello world") # prints some text to the screen
+def startingScreen(stdscr):
+    # clears screen from last run
+    stdscr.clear()
+    # prints welcome message to screen
+    stdscr.addstr("Welcome to the Typing Speed Test!")
+    stdscr.addstr("\nPress any key to begin: ") 
     stdscr.refresh()
-    stdscr.getkey() # makes sure that display on screen stays there and waits for user key entry
+    key = stdscr.getkey()
+    print(key)
 
+def main(stdscr):
+     # create curses color pairs foreground/background with different int ID's
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
+    startingScreen(stdscr)
+    
 wrapper(main)
